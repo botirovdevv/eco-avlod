89c424afcf0d5108d8500e2e589d8def2a4bb2d7
botirovdev <botirovdev7@gmail.com>
2025-02-05 21:29:07 +0500

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
 import About from './components/About'
-import Video from './components/Video'
 
 const App = () => {
   return (
@@ -10,7 +9,6 @@ const App = () => {
       <Navbar />
       <Header />
       <About/>
-      <Video/>
     </main>
   )
 }
