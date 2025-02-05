3131631f5193fb280b56c3bc8b3fbe2d5fac65dd
botirovdev <botirovdev7@gmail.com>
2025-02-05 21:19:11 +0500

    fixed bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,19 +9,19 @@ const About = () => {
                     <div className="about-box">
                         <img width={200} className='about-img' src={img} alt="" />
                         <p className="about-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus magni nobis accusamus blanditiis ducimus possimus totam voluptas provident porro. Nulla excepturi iusto perspiciatis inventore repellat tempora quisquam fugit error.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus magni nobis accusamus blanditiis ducimus possimus totam voluptas provident porro. 
                         </p>
                     </div>
                     <div className="about-box_2">
                         <p className="about-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus magni nobis accusamus blanditiis ducimus possimus totam voluptas provident porro. Nulla excepturi iusto perspiciatis inventore repellat tempora quisquam fugit error.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus magni nobis accusamus blanditiis ducimus possimus totam voluptas provident porro. 
                         </p>
                         <img width={200} className='about-img' src={img} alt="" />
                     </div>
                     <div className="about-box">
                         <img width={200} className='about-img' src={img} alt="" />
                         <p className="about-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus magni nobis accusamus blanditiis ducimus possimus totam voluptas provident porro. Nulla excepturi iusto perspiciatis inventore repellat tempora quisquam fugit error.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus magni nobis accusamus blanditiis ducimus possimus totam voluptas provident porro. 
                         </p>
                     </div>
 
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,12 +3,14 @@ import video from '../assets/images/video.mp4'
 
 const Video = () => {
     return (
-        <div className='container'>
-            <video width="100%" controls >
-                <source src={video} type="video/mp4" />
-                Sizning brauzeringiz video tagini qo‘llab-quvvatlamaydi.
-            </video>
-        </div>
+        <section className="video">
+            <div className='container'>
+                <video width="100%" controls className='video-player'>
+                    <source src={video} type="video/mp4" />
+                    Sizning brauzeringiz video tagini qo‘llab-quvvatlamaydi.
+                </video>
+            </div>
+        </section>
     )
 }
 
